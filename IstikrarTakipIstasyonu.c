58f8ac0a289821d719d7fc3f29c89c6cdb7f48cf
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-04-11 05:54:19 +0300

    fonksiyon ve yeni başlik ekledim

--- IstikrarTakipIstasyonu.c
+++ IstikrarTakipIstasyonu.c
@@ -1,23 +1,23 @@
 #include<Stdio.h>
+	int ilkFark,top=0,bA,sA,bG,sG,aydaGun,top2=0;
+   
 int main(){
 	// yeni eklemeler suan hangi dalda
 	baslangic:
 	printf("Istikrar takip istasyonuna hosgeldiniz\nKac numarali istikrari gormek istersiniz\n");
+	printf("1- lgs sýnavýna kalan gün\n4-Ramazan istikrarý \n9-Yabancý dil istikrarý\n8-Teknik beceri istikrarý ");
 	int anahtar;
 	scanf("%d",&anahtar);
-	int ilkFark,top=0,bA,sA,bG,sG,aydaGun,top2=0;
+	
+	printf("bugunun ay ve gununu yaziniz\n");
+	scanf("%d%d",&sA,&sG);
+    int d=sA;
 	
 	switch(anahtar){
 	
 	
 		case 1:
 		
-    bA=6;
-	bG=4;
-	printf("bugunun ay ve gununu yaziniz\n");
-	scanf("%d%d",&sA,&sG);
-    int d=sA;
-	
 	for(;bA-1>sA;sA++){
 		switch(sA+1){
 			case 1:
@@ -122,8 +122,50 @@ goto baslangic;
 		
     bA=3;
 	bG=23;
-	printf("bugunun ay ve gununu yaziniz\n");
-	scanf("%d%d",&sA,&sG);
+    gecenGun(sA,sG);
+goto baslangic;
+	break;
+	
+	case 5:	
+	
+    bA=4;
+	bG=10;
+    gecenGun(sA,sG);
+goto baslangic;
+	break;
+	
+	case 8:
+		
+	bA=2;
+	bG=14;
+		gecenGun(sA,sG);
+		goto baslangic;
+		break;
+		
+	case 9 :
+		
+	bA=1;
+	bG=10;
+	gecenGun(sA,sG);
+goto baslangic;
+	break; 
+	
+	
+		default:
+				printf("Yanlis giris yaptiniz tekrar giriniz\n");
+			goto baslangic;
+				break;
+}
+
+}
+void gecenGun(int sA, int sG){
+
+if(sA==bA){
+	top=sG-bG;
+	printf("%d  gunluk istikrar!\n",top);
+}
+else{
+
 
 	
 	for(;bA<sA-1;bA++){
@@ -166,64 +208,5 @@ goto baslangic;
 		
 	top=top2+ilkFark+sG;
 	printf("%d  gunluk istikrar!\n",top);
-goto baslangic;
-	break;
-	
-	case 9 :
-		
-	bA=1;
-	bG=10;
-	printf("bugunun ay ve gununu yaziniz\n");
-	scanf("%d%d",&sA,&sG);
-
-	
-	for(;bA<sA-1;bA++){
-		switch(bA+1){
-			case 1:
-				aydaGun=31;
-				break;
-			case 2:
-				aydaGun=28;
-				break;
-			case 3:
-				aydaGun=31;
-				break;
-			case 4:
-				aydaGun=30;
-				break;
-			default:
-				printf("Yanlis giris yaptiniz tekrar giriniz\n");
-				break;
-		} // ne olmus burda
-		    top2+=aydaGun;
-	} 
-	switch(bA){
-			case 1:
-				aydaGun=31;ilkFark=aydaGun-bG;
-				break;
-			case 2:
-				aydaGun=28;ilkFark=aydaGun-bG;
-				break;
-			case 3:
-				aydaGun=31;ilkFark=aydaGun-bG;
-				break;
-			case 4:
-				aydaGun=30;ilkFark=aydaGun-bG;
-				break;
-			default:
-				printf("Yanlis giris yaptiniz tekrar giriniz\n");
-				break;
-		}
-		
-	top=top2+ilkFark+sG;
-	printf("%d : gunluk istikrar!\n",top);
-goto baslangic;
-	break; 
-	
-	
-		default:
-				printf("Yanlis giris yaptiniz tekrar giriniz\n");
-			goto baslangic;
-				break;
-}
+	}
 }
